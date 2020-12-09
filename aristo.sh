bc6037fe8a95ea321dc7241a391ed4926ff3436c
tim born <timborn@deltacfax.com>
2020-12-08 19:58:25 -0700

    add dividend aristocrats

---
+++ aristo.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# extra a subset using shell variable for stock symbol
+
+for i in `cat aristocrats`; do
+	stock-stats $i | jq '{ symbol: "'$i'", price: .Price, dividend: .Dividend, dividendPct: ."Dividend %" }' 2>/dev/null
+done
