978303db6c544b7cc7b2e988dbc9390e538a8eb6
tim born <timborn@deltacfax.com>
2020-12-15 15:59:22 -0700

    sectors.sh now accepts optional file name which lists stock symbols

--- sectors.sh
+++ sectors.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 # generate the unique list of all sectors across aristocrats
 
-for i in `cat aristocrats`; do
+# OPTIONAL: name of a file to use instead of aristocrats
+# the file is just a list of stock symbols, one per line
+FN="aristocrats"
+if [ $# -eq 1 ]; then
+	if [ -r $1 ] ; then 
+		FN=$1
+	fi
+fi
+
+for i in `cat $FN`; do
 	stock-stats $i | jq '.Sector'
 done | sort -u
