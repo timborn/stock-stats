9522a2119c374177f852e88ea8ac338bbd68d8fd
tim born <timborn@deltacfax.com>
2020-12-20 14:46:35 -0700

    add -n flag to aristo.sh to supress filtering out some of the stocks

--- aristo.sh
+++ aristo.sh
@@ -4,6 +4,22 @@
 # to consider and filters the fields we care about.  Basically all the
 # other scripts depend on that field selection.
 # HA!  payout is the inverse of divCover.  Didn't think this through and added divCover, but really not needed
+# Sometimes we do not want to filter things out, in which case -n flag is useful.
+
+USAGE="USAGE: $0 [-n] [file]"
+NOFILT=0
+while getopts ":n" opt; do
+  case ${opt} in 
+    n ) # no filters
+      NOFILT=1
+      shift
+      ;;
+    \? )
+      echo $USAGE
+      exit 1
+      ;;
+  esac
+done
 
 # OPTIONAL: name of a file to use instead of aristocrats
 # the file is just a list of stock symbols, one per line
@@ -26,5 +42,5 @@ for i in `cat $FN`; do
 	./calculate-target-gains.js |
 	./calculate-div-cover.js |
 	./midprice.js |
-	./filter-out-payout.js		# toss anything where payout ratio >= 100% 
+	if [ $NOFILT -eq 0 ] ; then ./filter-out-payout.js; else cat ; fi	# toss anything where payout ratio >= 100% 
 done
