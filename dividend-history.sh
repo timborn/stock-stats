674aba72257630197e20b21753d47d20fec02ee6
tim born <timborn@deltacfax.com>
2020-12-23 09:07:34 -0700

    found and fixed bug in dividend-history.sh.  added a ton of debug

--- dividend-history.sh
+++ dividend-history.sh
@@ -1,5 +1,6 @@
 # NB set TOKEN before running
 # SYM=MMM
+# DEBUG=1		# when DEBUG is unset, it is turned off
 CACHE=$HOME/.iex-cache
 LOG=$CACHE/LOG
 
@@ -12,7 +13,10 @@ fi
 # return 0 if file too old (or doesn't exit)
 # https://gist.github.com/ashrithr/5614283
 function fileTooOld() {
+  [ $DEBUG ] && echo "fileTooOld entry ..."
+
   if [ ! -f $CACHE/$1 ]; then
+    [ $DEBUG ] && echo "fileTooOld($1) not in cache, returning 0 (too old, refresh cache)"
     return 0
   fi
 
@@ -20,10 +24,14 @@ function fileTooOld() {
   MAXAGE=7776000	# 90 days
 
   # file age in seconds = current_time - file_modification_time.
-  FILEAGE=$(($(date +%s) - $(stat -f '%m' "$1")))
+  FILEAGE=$(($(date +%s) - $(stat -f '%m' "$CACHE/$1")))
   test $FILEAGE -lt $MAXAGE && {
+    [ $DEBUG ] && echo FILEAGE=$FILEAGE
+    [ $DEBUG ] && echo MAXAGE=$MAXAGE
+    [ $DEBUG ] && echo "DEBUG: we believe $SYM is LESS THAN max age (remember: fileTooOld, therefore we return 1 --> false/fail)"
     return 1
   }
+  [ $DEBUG ] && "fileTooOld falling off the bottom, returning 0 (true/success)"
   return 0
 }
 
@@ -40,6 +48,10 @@ fi
 
 fileTooOld $SYM 
 RETVAL=$?
+
+# [ $DEBUG ] && echo "fileTooOld returned $RETVAL.  Forcing to 1." ; RETVAL=1
+[ $DEBUG ] && echo "fileTooOld returned $RETVAL." 
+
 if [ $RETVAL -eq 0 ] ; then 
 	# cache it
 	RETVAL=$( curl -s -o $CACHE/$SYM -w "%{http_code}" https://cloud.iexapis.com/stable/stock/$SYM/dividends/5y?token=$TOKEN )
