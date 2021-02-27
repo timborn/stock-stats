e2144d557b6c6d55d2be481278ef20350d99d79f
tim born <timborn@deltacfax.com>
2021-02-27 13:45:00 -0700

    added divcon to advise.sh

--- advise.sh
+++ advise.sh
@@ -2,11 +2,10 @@
 
 # ctrl-cmd-space to find emojis
 ALERT="❗️"
-echo $ALERT
-echo ❗️
-echo ✅
 OK="✅"
-echo $OK
+
+# get TICKER off cmdline
+TICKER=T
 
 # did this stock actually have any earnings?
 # is the price "fair" (at or below midpoint of past year hi/lo)
@@ -26,3 +25,18 @@ echo $OK
 # Expect a quick ratio of 1.0.  Numbers below this suggest the 
 # company may have trouble paying liabilities without taking on
 # more debt or selling assets.
+
+# divcon >= 3
+LASTUPDATE=`head -1 divcontable.csv  | cut -f 7 -d,`
+DIVCON=`grep ,$TICKER, divcontable.csv  | cut -f7 -d,`
+if [ $DIVCON -ge 3 ] ; then
+	echo -n $OK 
+else
+	echo -n $ALERT
+fi
+echo DIVCON: $DIVCON
+echo DIVCON uses a five-tier rating, from 1 to 5, to gauge companies\'
+echo dividend health. A DIVCON 5 rating indicates not just a healthy
+echo dividend, but a high likelihood of dividend growth. DIVCON 1 dividend
+echo stocks, on the other hand, are the likeliest to cut or suspend their
+echo payouts.
