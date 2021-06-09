ffe94febe4f5b3ddf8114bb270893d299f2b210d
tim born <timborn@deltacfax.com>
2021-06-09 16:18:38 -0700

    worth-a-look.sh was not running correctly from cron as the PATH was different

--- worth-a-look.sh
+++ worth-a-look.sh
@@ -1,5 +1,8 @@
 # capture recommendations.  runs out of crontab once a month
 
+# pick up jq, node, stock-stats
+PATH=$PATH:/usr/local/bin:~timborn/bin
+
 cd `dirname $0`	# be there, aloha!
 
 OFILE=`date "+worth-a-look.%y%m%d"`
