75981d8d9ded0487728e8e36057d31eaeb44b058
tim born <timborn@deltacfax.com>
2021-10-19 08:35:48 -0500

    stinky hack to worth-a-look.sh until I can sort out PATHs

--- worth-a-look.sh
+++ worth-a-look.sh
@@ -6,4 +6,11 @@ PATH=$PATH:/usr/local/bin:~timborn/bin
 cd `dirname $0`	# be there, aloha!
 
 OFILE=`date "+worth-a-look.%y%m%d"`
-/Volumes/BlueMountain/Users/timborn/bash/stock-stats/recommend.sh > $OFILE
+
+# this sucks    TODO: fix this
+HN=`hostname -s`
+if [ $HN = "Timex-3" ] ; then
+	/Users/timborn/python/stock-stats/recommend.sh > $OFILE
+else
+	/Volumes/BlueMountain/Users/timborn/bash/stock-stats/recommend.sh > $OFILE
+fi
