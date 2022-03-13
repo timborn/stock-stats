b036b0f003f8d713fb8ef3c276e2edbe60c97c0b
tim born <timborn@deltacfax.com>
2022-03-13 16:43:06 -0700

    removed hardcoded references to 2016 & 2021; trying to harden buggy code in 5-yr-div-history.sh

--- 5-yr-div-history.sh
+++ 5-yr-div-history.sh
@@ -16,6 +16,7 @@ jq -sc |
 in2csv -f json  | 
 tail -n +2 |
 sed -e's/-[0-9][0-9]-[0-9][0-9]//' |  
-awk -F ',' '{a[$1] += $2} END{if (!(2016 in a)) print "BUG: 2016 missing from data";  delete a[2021]; for (i in a) print i, a[i]}'
+awk -F ',' '{a[$1] += $2} END{ for (i in a) print i, a[i] }'
+# awk -F ',' '{a[$1] += $2} END{if (!(2016 in a)) print "BUG: 2016 missing from data";  delete a[2021]; for (i in a) print i, a[i]}'
 # awk -F ',' '{a[$1] += $2} END{print 2016, a[2016]; print 2017, a[2017]; print 2018, a[2018]; print 2019, a[2019]; print 2020, a[2020] }'
 
